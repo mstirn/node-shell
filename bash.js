c5f21b644e1323149bb774e488fb3ab695f34f3a
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-03-06 13:41:23 -0500

    start cat.js

--- bash.js
+++ bash.js
@@ -3,19 +3,25 @@ process.stdout.write('prompt > ');
 
 //stdin
 process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim().split(' ');
+
     //command function variables
-    const cmd = data.toString().trim();
     const pwd = require("./pwd");
     const ls = require("./ls");
+    const cat = require("./cat");
     //---------------------------
 
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('You typed: ' + cmd[0]);
 
-    if (cmd === "pwd") {
+    if (cmd[0] === "pwd") {
         pwd();
     }
 
-    if (cmd === "ls") {
+    if (cmd[0] === "ls") {
+        ls();
+    }
+
+    if (cmd[0] === "cat") {
         ls();
     }
 
---
+++ cat.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+module.exports = function (arg) {
+    fs.readFile
+    process.stdout.write('\n' + process.cwd());
+    process.stdout.write('\nprompt > ');
+};
